64048151f0f4894b744cc46fc0145ca23cfe248a
alex <alex@Debian.myguest.virtualbox.org>
2023-04-17 23:10:51 +0300

    add code to compose and app

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from redis import Redis
 import socket
 
 app = Flask(__name__)
-
+redis = Redis(host='redis',port=6379)
 @app.route('/')
 def hello():
-  return "Hello from host.<p>HOstname is: " + socket.gethostname()
+  count = redis.incr('hitpoints')
+  return "Hello from host.<p>HOstname is: " + socket.gethostname() + '\n I count {} times'.format(count)
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=8000, debug=True)
